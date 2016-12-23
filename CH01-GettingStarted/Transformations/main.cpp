69d48f7b157f54338c04bcb2aa6de5b55046cc0f
yanjunyang.yyj <yanjunyang.yyj@alibaba-inc.com>
2016-12-23 11:47:22 +0800

    20161222-Getting Started-Transformations

---
+++ CH01-GettingStarted/Transformations/main.cpp
@@ -0,0 +1,182 @@
+// Preprocessor Directives
+#define STB_IMAGE_IMPLEMENTATION
+
+// Local Headers
+#include "glitter.hpp"
+#include "shader.h"
+
+// Console Color
+#include "consoleColor.hpp"
+
+// System Headers
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+// Standard Headers
+//#include <cstdio>
+//#include <cstdlib>
+#include <iostream>
+
+// 声明按键函数
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+int main(int argc, char * argv[]) {
+
+    // glfw初始化
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // 此行用来给Mac OS X系统做兼容
+    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+    // 创建窗口,获取窗口上上下文
+    GLFWwindow* window = glfwCreateWindow(mWidth, mHeight, "LearnOpenGL", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // 通过glfw注册按键事件回调
+    glfwSetKeyCallback(window, key_callback);
+
+    // Load OpenGL Functions
+    gladLoadGL();
+//    fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+    std::cout << BLUE << "OpenGL " << glGetString(GL_VERSION) << RESET << std::endl;
+
+    // 查询GPU最大支持顶点个数
+//    GLint nrAttributes;
+//    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+//    std::cout << GREEN << "Maximum nr of vertex attributes supported: " << nrAttributes << RESET << std::endl;
+
+    // 获取视口
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    // 编译着色器程序
+    Shader ourShader("vert.vert", "frag.frag");
+
+    // 顶点输入
+    GLfloat vertices[] = {
+//     ---- 位置 ----       ---- 颜色 ----     - 纹理坐标 -
+            0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // 右上
+            0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 右下
+            -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // 左下
+            -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // 左上
+    };
+
+    GLuint indices[] = { // 注意索引从0开始!
+            0, 1, 3, // 第一个三角形
+            1, 2, 3  // 第二个三角形
+    };
+
+
+    // 顶点数组对象 Vertex Array Object, VAO
+    // 顶点缓冲对象 Vertex Buffer Object，VBO
+    // 索引缓冲对象：Element Buffer Object，EBO或Index Buffer Object，IBO
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    // !!! Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+    glBindVertexArray(VAO);
+    // 把顶点数组复制到缓冲中供OpenGL使用
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // 设置顶点position属性指针
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)0);
+    glEnableVertexAttribArray(0);
+
+    // 设置顶点color属性指针
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(1);
+
+    // 设置顶点TexCoord属性指针
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (GLvoid*)(6 * sizeof(GLfloat)));
+    glEnableVertexAttribArray(2);
+
+    glBindVertexArray(0);
+
+    // 使用线框模式进行渲染
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // 读取并创建贴图
+    GLuint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // 之后对GL_TEXTURE_2D的所以操作都作用在texture对象上
+    // 设置纹理环绕方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // 设置纹理过滤方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // 读取图片并生成纹理映射
+//    int width, height;
+    unsigned char* image = stbi_load("wall.jpg", &width, &height, 0, 0);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(image);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+
+    // 变换矩阵
+    glm::mat4 trans;
+    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5)); //缩放
+    trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f)); //位移
+
+    // 渲染
+    while (!glfwWindowShouldClose(window))
+    {
+        // 检查事件
+        glfwPollEvents();
+
+        // 渲染指令
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // 绑定texture
+        glBindTexture(GL_TEXTURE_2D, texture);
+
+        // 绘图
+        ourShader.Use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // 变换
+        trans = glm::rotate(trans, (GLfloat)glfwGetTime() * 5.0f, glm::vec3(0.0f, 0.0f, 1.0f)); //随时间旋转
+        GLuint transformLoc = glGetUniformLocation(ourShader.Program, "transform");
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+        // 交换缓冲
+        glfwSwapBuffers(window);
+    }
+
+    // 释放VAO,VBO
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // 释放GLFW分配的内存
+    glfwTerminate();
+
+    return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // 当用户按下ESC键,我们设置window窗口的WindowShouldClose属性为true
+    // 关闭应用程序
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
